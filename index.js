65a49d08131a7ab42b5270a3af78130360970deb
Glen <glen.kimani@student.moringaschool.com>
2022-12-09 12:35:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+function reverseString(word) {
+  {
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+}
+
+
+
+
+
 function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+  
   // Write your algorithm here
 }
 
